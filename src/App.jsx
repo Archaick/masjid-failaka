ba07f7333073a767a5da1df8e28fd679adf0c7f8
Archaick <saeed.jh3@gmail.com>
2024-11-03 15:00:39 +0700

    mantine componenets erased

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import "./App.css";
-import { HeaderMenu } from "./components/HeaderMenu";
 
 function App() {
   return (
     <MantineProvider>
-      <HeaderMenu />
     </MantineProvider>
   );
 }
