b0fe836eec656613229c36a02008d0644ebbcd5e
Archaick <saeed.jh3@gmail.com>
2024-11-04 17:49:19 +0700

    mantine reinstalled

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
 import "./App.css";
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 function App() {
   return (
     <MantineProvider>
+        <div>
+          Hello, world!
+        </div>
     </MantineProvider>
   );
 }
