0a61168345d9c0fd91c25a1d41ceab6a82bbe897
Archaick <saeed.jh3@gmail.com>
2024-11-28 21:44:47 +0700

    add padding and centering text Panel

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-import { Tabs, Box, Title, Text } from "@mantine/core";
+import { Tabs, Box, Title, Text, Center } from "@mantine/core";
 import "./History.css";
-import { IconArrowUp, IconPhoto, IconTimeline, IconUser } from '@tabler/icons-react';
+import {
+  IconArrowUp,
+  IconPhoto,
+  IconTimeline,
+  IconUser,
+} from "@tabler/icons-react";
 
 const History = () => {
   return (
@@ -11,7 +16,12 @@ const History = () => {
         Our History
       </Title>
 
-      <Tabs color="#4ecca3" defaultValue="founder" orientation="vertical" className="history-tabs">
+      <Tabs
+        color="#4ecca3"
+        defaultValue="founder"
+        orientation="vertical"
+        className="history-tabs"
+      >
         <Tabs.List>
           <Tabs.Tab value="founder">
             <IconUser />
@@ -32,34 +42,55 @@ const History = () => {
         </Tabs.List>
 
         {/* Tab Panels */}
-        <Tabs.Panel value="founder">
-          <Text className="tab-content">
-            Established in 2000 through a generous donation by Kuwaiti philanthropist Fatima Jasim, Masjid Failaka serves as a central place of worship, learning, and community in Palemrah, Jakarta.
-          </Text>
-        </Tabs.Panel>
+        <Center>
+          <Tabs.Panel value="founder">
+            <Text className="tab-content">
+              Founded in 2000 through a generous donation from Kuwaiti
+              philanthropist Fatima Jasim, Masjid Failaka is a center for worship,
+              learning, and community in Palemrah, Jakarta.
+            </Text>
+          </Tabs.Panel>
+        </Center>
 
-        <Tabs.Panel value="milestones">
-          <Text className="tab-content">
-            <ul>
-              <li>Built to accommodate the local community's spiritual needs.</li>
-              <li>Hosts daily prayers, Quran study sessions, and religious events.</li>
-              <li>Offers youth and educational programs for personal and spiritual growth.</li>
-              <li>In 2020, celebrated two decades of serving the Palemrah community.</li>
-            </ul>
-          </Text>
-        </Tabs.Panel>
+        <Center>
+          <Tabs.Panel value="milestones">
+            <Text className="tab-content">
+              <ul>
+                <li>
+                  Hosts daily prayers, Quran study sessions, and religious
+                  events.
+                </li>
+                <li>
+                  Offers youth and educational programs for personal and
+                  spiritual growth.
+                </li>
+                <li>
+                  In 2020, celebrated two decades of serving the Palemrah
+                  community.
+                </li>
+              </ul>
+            </Text>
+          </Tabs.Panel>
+        </Center>
 
-        <Tabs.Panel value="gallery">
-          <Text className="tab-content">
-            Explore photos and videos of events, daily prayers, and special moments that have brought the Palemrah community closer together.
-          </Text>
-        </Tabs.Panel>
+        <Center>
+          <Tabs.Panel value="gallery">
+            <Text className="tab-content">
+              Explore photos and videos of events, daily prayers, and special
+              moments that have brought the Palemrah community closer together.
+            </Text>
+          </Tabs.Panel>
+        </Center>
 
-        <Tabs.Panel value="future">
-          <Text className="tab-content">
-            As we look to the future, our focus is on continuing to serve the Palemrah community by offering programs for youth, enhancing facilities, and fostering a spirit of togetherness and faith.
-          </Text>
-        </Tabs.Panel>
+        <Center>
+          <Tabs.Panel value="future">
+            <Text className="tab-content">
+              Our future focus is on serving the Palemrah community by expanding
+              youth programs, improving facilities, and nurturing unity and
+              faith.
+            </Text>
+          </Tabs.Panel>
+        </Center>
       </Tabs>
     </Box>
   );
